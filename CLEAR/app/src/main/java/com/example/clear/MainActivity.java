dc4e97aa3a96fb313e5fe6f61610cd3ee8bf598b
zhangyuxin <1069866826@qq.com>
2020-05-06 22:36:17 +0800

    Amap initial && location

--- CLEAR/app/src/main/java/com/example/clear/MainActivity.java
+++ CLEAR/app/src/main/java/com/example/clear/MainActivity.java
@@ -1,14 +1,335 @@
 package com.example.clear;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.CameraUpdateFactory;
+import com.amap.api.maps2d.LocationSource;
+import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.UiSettings;
+import com.amap.api.maps2d.model.BitmapDescriptorFactory;
+import com.amap.api.maps2d.model.MyLocationStyle;
+
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements AMapLocationListener{
+
+    /*
+    private MapView mapView;
+    //定位服务类。此类提供单次定位、持续定位、地理围栏、最后位置相关功能
+    private AMapLocationClient aMapLocationClient;
+    private OnLocationChangedListener listener;
+    //定位参数设置
+    private AMapLocationClientOption aMapLocationClientOption;
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mapView = (MapView)findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);  //必须写
+        AMap aMap = mapView.getMap();
+        //设置地图类型
+        aMap.setMapType(AMap.MAP_TYPE_NORMAL);
+
+        MyLocationStyle locationStyle = new MyLocationStyle();
+        aMap.setMyLocationStyle(locationStyle);
+
+        // 设置定位监听
+        aMap.setLocationSource(this);
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示
+        aMap.setMyLocationEnabled(true);// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+        // 设置定位的类型为定位模式，参见类AMap。
+//        aMap.setMyLocationType(AMap.LOCATION_TYPE_LOCATE);
+        // 设置为true表示系统定位按钮显示并响应点击，false表示隐藏，默认是false
+        aMap.setMyLocationEnabled(true);
+
+        aMapLocationClient = new AMapLocationClient(getApplicationContext());
+        aMapLocationClient.setLocationListener(this);
+
+        //初始化定位参数
+        aMapLocationClientOption = new AMapLocationClientOption();
+        //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+        aMapLocationClientOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //设置是否返回地址信息（默认返回地址信息）
+        aMapLocationClientOption.setNeedAddress(true);
+        //设置是否只定位一次,默认为false
+        aMapLocationClientOption.setOnceLocation(false);
+        //设置是否强制刷新WIFI，默认为强制刷新
+        aMapLocationClientOption.setWifiActiveScan(true);
+        //设置是否允许模拟位置,默认为false，不允许模拟位置
+        aMapLocationClientOption.setMockEnable(false);
+        //设置定位间隔,单位毫秒,默认为2000ms
+        aMapLocationClientOption.setInterval(2000);
+        //给定位客户端对象设置定位参数
+        aMapLocationClient.setLocationOption(aMapLocationClientOption);
+        //启动定位
+        aMapLocationClient.startLocation();
+    }
+
+
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        if(listener!=null && aMapLocation!=null) {
+            listener.onLocationChanged(aMapLocation);// 显示系统小蓝点
+            if (aMapLocation.getErrorCode() == 0) {
+                //定位成功回调信息，设置相关消息
+                aMapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
+                aMapLocation.getLatitude();//获取经度
+                aMapLocation.getLongitude();//获取纬度;
+                aMapLocation.getAccuracy();//获取精度信息
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Date date = new Date(aMapLocation.getTime());
+                df.format(date);//定位时间
+                aMapLocation.getAddress();//地址，如果option中设置isNeedAddress为false，则没有此结果
+                aMapLocation.getCountry();//国家信息
+                aMapLocation.getProvince();//省信息
+                aMapLocation.getCity();//城市信息
+                aMapLocation.getDistrict();//城区信息
+                aMapLocation.getRoad();//街道信息
+                aMapLocation.getCityCode();//城市编码
+                aMapLocation.getAdCode();//地区编码
+            } else {
+                //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+                Log.e("Tomato","location Error, ErrCode:"
+                        + aMapLocation.getErrorCode() + ", errInfo:"
+                        + aMapLocation.getErrorInfo());
+            }
+        }
+    }
+
+
+    @Override
+    public void activate(OnLocationChangedListener onLocationChangedListener) {
+        listener = onLocationChangedListener;
+    }
+
+    @Override
+    public void deactivate() {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+        //销毁定位客户端
+        if(aMapLocationClient!=null){
+            aMapLocationClient.onDestroy();
+            aMapLocationClient = null;
+            aMapLocationClientOption = null;
+        }
+    }
+    */
+
+
+    private MapView mapView;
+    private AMap aMap;
+
+    private static final int MY_PERMISSIONS_REQUEST_CALL_LOCATION = 1;
+    public AMapLocationClient mlocationClient;
+    public AMapLocationClientOption mLocationOption = null;
+
+    //定位蓝点
+    MyLocationStyle myLocationStyle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapView = (MapView)findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        init();
+
+        //设置地图的放缩级别
+        aMap.moveCamera(CameraUpdateFactory.zoomTo(16));
+
+        //蓝点初始化
+        myLocationStyle = new MyLocationStyle();
+        myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+//        aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
+        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+        myLocationStyle.showMyLocation(true);
+
+        //定义一个UiSettings对象
+        UiSettings mUiSettings = aMap.getUiSettings();//实例化UiSettings类对象
+//        aMap.setLocationSource((LocationSource) this);//通过aMap对象设置定位数据源的监听
+        mUiSettings.setMyLocationButtonEnabled(true); //显示默认的定位按钮
+        aMap.setMyLocationEnabled(true);// 可触发定位并显示当前位置
+
+        //检查版本是否大于M
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_CALL_LOCATION);
+            } else {
+                //"权限已申请";
+                showLocation();
+            }
+        }
+    }
+
+    private void init(){
+        if(aMap ==null){
+            aMap = mapView.getMap();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_CALL_LOCATION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //"权限已申请"
+                showLocation();
+            } else {
+                showToast("权限已拒绝,不能定位");
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    // TODO:
+    private void showLocation() {
+        try {
+            mlocationClient = new AMapLocationClient(this);
+            mLocationOption = new AMapLocationClientOption();
+            mlocationClient.setLocationListener(this);
+            //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            mLocationOption.setInterval(5000);
+            //设置定位参数
+            mlocationClient.setLocationOption(mLocationOption);
+            //启动定位
+            mlocationClient.startLocation();
+            Log.i("showlocation","success");
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mapView.onPause();
+        // 停止定位
+        if (null != mlocationClient) {
+            mlocationClient.stopLocation();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState){
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        mapView.onDestroy();
+        if (null != mlocationClient) {
+            mlocationClient.onDestroy();
+            mlocationClient = null;
+        }
+    }
+
+
+    @Override
+    public void onLocationChanged(AMapLocation amapLocation) {
+        try {
+            if (amapLocation != null) {
+                if (amapLocation.getErrorCode() == 0) {
+                    //定位成功回调信息，设置相关消息
+
+                    //获取当前定位结果来源，如网络定位结果，详见定位类型表
+                    Log.i("定位类型", amapLocation.getLocationType() + "");
+                    Log.i("获取纬度", amapLocation.getLatitude() + "");
+                    Log.i("获取经度", amapLocation.getLongitude() + "");
+                    Log.i("获取精度信息", amapLocation.getAccuracy() + "");
+
+                    //如果option中设置isNeedAddress为false，则没有此结果，网络定位结果中会有地址信息，GPS定位不返回地址信息。
+                    Log.i("地址", amapLocation.getAddress());
+                    Log.i("国家信息", amapLocation.getCountry());
+                    Log.i("省信息", amapLocation.getProvince());
+                    Log.i("城市信息", amapLocation.getCity());
+                    Log.i("城区信息", amapLocation.getDistrict());
+                    Log.i("街道信息", amapLocation.getStreet());
+                    Log.i("街道门牌号信息", amapLocation.getStreetNum());
+                    Log.i("城市编码", amapLocation.getCityCode());
+                    Log.i("地区编码", amapLocation.getAdCode());
+                    Log.i("获取当前定位点的AOI信息", amapLocation.getAoiName());
+                    Log.i("获取当前室内定位的建筑物Id", amapLocation.getBuildingId());
+                    Log.i("获取当前室内定位的楼层", amapLocation.getFloor());
+                    Log.i("获取GPS的当前状态", amapLocation.getGpsAccuracyStatus() + "");
+
+                    //获取定位时间
+                    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    Date date = new Date(amapLocation.getTime());
+
+                    Log.i("获取定位时间", df.format(date));
+
+
+                    // 停止定位
+                    mlocationClient.stopLocation();
+                } else {
+                    //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+                    Log.e("AmapError", "location Error, ErrCode:"
+                            + amapLocation.getErrorCode() + ", errInfo:"
+                            + amapLocation.getErrorInfo());
+                }
+            }
+        } catch (Exception e) {
+        }
+    }
+
+    private void showToast(String string) {
+        Toast.makeText(MainActivity.this, string, Toast.LENGTH_LONG).show();
     }
 }
