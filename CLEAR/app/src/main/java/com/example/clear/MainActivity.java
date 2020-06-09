ad2338c23b5a1dec101f94f8bf145cbc520a2c87
zhangyuxin <1069866826@qq.com>
2020-06-10 01:37:07 +0800

    代码结构优化

--- CLEAR/app/src/main/java/com/example/clear/MainActivity.java
+++ CLEAR/app/src/main/java/com/example/clear/MainActivity.java
@@ -103,17 +103,9 @@ public class MainActivity extends AppCompatActivity
 
     //定位蓝点
     MyLocationStyle myLocationStyle;
-    //地点搜索
-    private String keyWord = "";// 要输入的poi搜索关键字
-    private PoiResult poiResult; // poi返回的结果
-    private int currentPage = 0;// 当前页面，从0开始计数
-    private PoiSearch.Query query;// Poi查询条件类
-    private PoiSearch poiSearch;// POI搜索
 
     // 组件
     private AutoCompleteTextView searchText;// 输入搜索关键字
-    private TextView editCity;// 要输入的城市名字或者城市区号
-    private ListView poiListView;
     TextView startTimeView, endTimeView, timePeriodView;  // 输入组件
     private Calendar cal;   //当前时间
     Toolbar toolbar;
@@ -122,7 +114,6 @@ public class MainActivity extends AppCompatActivity
     private FragmentTransaction fragmentTransaction;  //Fragment 事务处理
     PoiSearchFragment fragment1;
 
-    private boolean poisitionIsChosen;    // 判断是否已经选择地点
     String nowCity; //现在定位的城市
     double nowLat, nowLon;  //现在定位的经纬度
 
@@ -142,7 +133,6 @@ public class MainActivity extends AppCompatActivity
 
         mapView = findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);
-        poiListView = findViewById(R.id.poi_list);
         loginState();
         mapInit();
         getDate();
@@ -159,11 +149,6 @@ public class MainActivity extends AppCompatActivity
                 showLocation();
             }
         }
-
-//        @Override
-//        public void thanks(String code) {
-//            // TODO Auto-generated method stub
-//            Toast.makeText(this, "已收到Fragment的消息：--"+code+"--,客气了", Toast.LENGTH_SHORT).show();
 
         FloatingActionButton fab=findViewById(R.id.fab_addTask);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -518,111 +503,6 @@ public class MainActivity extends AppCompatActivity
     }
 
     /**
-     *开始进行poi搜索
-     */
-    /*
-    protected void doSearchQuery() {
-
-        currentPage = 0;
-        query = new PoiSearch.Query(keyWord, "", nowCity);// 第一个参数表示搜索字符串，第二个参数表示poi搜索类型，第三个参数表示poi搜索区域（空字符串代表全国）
-        query.setPageSize(10);// 设置每页最多返回多少条poiitem
-        query.setPageNum(currentPage);// 设置查第一页
-
-        poiSearch = new PoiSearch(this, query);
-        poiSearch.setOnPoiSearchListener(this);
-        poiSearch.searchPOIAsyn();
-
-    }
-*/
-    /**
-     *poi没有搜索到数据，返回一些推荐城市的信息
-     */
-    /*
-    private void showSuggestCity(List<SuggestionCity> cities) {
-        String infomation = "推荐城市\n";
-        for (int i = 0; i < cities.size(); i++) {
-            infomation += "城市名称:" + cities.get(i).getCityName() + "城市区号:"
-                    + cities.get(i).getCityCode() + "城市编码:"
-                    + cities.get(i).getAdCode() + "\n";
-        }
-        showToast(infomation);
-    }
-*/
-    /*
-    @Override
-    public void onPoiSearched(PoiResult result, int rCode) {
-        if (rCode == AMapException.CODE_AMAP_SUCCESS) {
-            if (result != null && result.getQuery() != null) {// 搜索poi的结果
-                if (result.getQuery().equals(query)) {// 是否是同一条
-                    poiResult = result;
-                    // 取得搜索到的poiitems有多少页
-                    final List<PoiItem> poiItems = poiResult.getPois();// 取得第一页的poiitem数据，页数从数字0开始
-
-                    //将list数据传到poi list fragment中
-                    List poiListResult = new ArrayList<>();
-                    for (int i=0; i<poiItems.size(); i++){
-                        String item=poiItems.get(i).toString();
-//                        Log.i("poiItem"+i, item);
-                        poiListResult.add(item);
-                    }
-                    poisitionIsChosen=false;    // listview点击事件初始为false
-//                    Log.i("check line", "poi search");
-
-                    ArrayAdapter<String> adapter = new ArrayAdapter<>
-                            (this, android.R.layout.simple_list_item_1, poiListResult);
-                    poiListView.setAdapter(adapter);
-                    //设置listview点击事件
-                    poiListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                        @Override
-                        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                            PoiItem item = poiItems.get(i);
-//                            Toast.makeText(MainActivity.this, item.toString(), Toast.LENGTH_SHORT).show();
-
-                            Log.i("choose position",
-                                    item.getPoiId()+" "+item.getLatLonPoint().getLatitude()+" "+item.getLatLonPoint().getLongitude()+" "+item.getTitle()+" "+item.getCityName());
-
-                            // 存储搜索的地点信息
-                            focusPoi=new PositionInfo();
-                            focusPoi.setPositionID(item.getPoiId());
-                            focusPoi.setPositionName(item.getTitle());
-                            focusPoi.setLatitude(item.getLatLonPoint().getLatitude());
-                            focusPoi.setLongitude(item.getLatLonPoint().getLongitude());
-                            focusPoi.setCity(item.getCityName());
-
-                            poisitionIsChosen=true;
-                            searchText.setText(item.toString());
-                        }
-                    });
-
-                    // 当搜索不到poiitem数据时，会返回含有搜索关键字的城市信息
-                    List<SuggestionCity> suggestionCities = poiResult.getSearchSuggestionCitys();
-                    if (poiItems != null && poiItems.size() > 0) {
-//                        aMap.clear();// 清理之前的图标
-//                        PoiOverlay poiOverlay = new PoiOverlay(aMap, poiItems);
-//                        poiOverlay.removeFromMap();
-//                        poiOverlay.addToMap();
-//                        poiOverlay.zoomToSpan();
-                    } else if (suggestionCities != null
-                            && suggestionCities.size() > 0) {
-                        showSuggestCity(suggestionCities);
-                    } else {
-                        showToast("没有结果");
-                    }
-                }
-            } else {
-                showToast("没有结果");
-            }
-        } else {
-            showToast(rCode+"");
-        }
-    }
-
-    @Override
-    public void onPoiItemSearched(PoiItem poiItem, int i) {
-
-    }
-*/
-    /**
      *设置页面监听
      */
     private void setUpViewListener() {
@@ -632,19 +512,12 @@ public class MainActivity extends AppCompatActivity
         userButton.setOnClickListener(this);
         Button searButton = findViewById(R.id.search_button);
         searButton.setOnClickListener(this);
-//        Button nextButton = findViewById(R.id.next_button);
-//        nextButton.setOnClickListener(this);
         Button searchIconButton =findViewById(R.id.search_icon);
         searchIconButton.setOnClickListener(this);
         Button changeToListButton =findViewById(R.id.change_to_list);
         changeToListButton.setOnClickListener(this);
         Button mapIconButton =findViewById(R.id.map_icon);
         mapIconButton.setOnClickListener(this);
-
-//        searchText = findViewById(R.id.keyWord);
-//        searchText.addTextChangedListener(this);// 添加文本输入框监听事件
-//        editCity = findViewById(R.id.city);
-//        editCity.setText(nowCity);  // 城市默认为当前定位的城市
 
         startTimeView=findViewById(R.id.start_time);
         startTimeView.setOnClickListener(this);
@@ -707,40 +580,6 @@ public class MainActivity extends AppCompatActivity
         return super.onKeyDown(keyCode, event);
     }
 
-    /**
-     * 点击空白处隐藏Listview
-     */
-    @Override
-    public boolean dispatchTouchEvent(MotionEvent ev) {
-
-        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
-            // 隐藏listview布局
-
-            ListView view=poiListView;
-            LinearLayout layout=findViewById(R.id.search_input);
-
-            int[] location = {0, 0};
-            int[] location2={0,0};
-
-            // 获取当前view在屏幕中离四边的边距
-            view.getLocationInWindow(location);
-            layout.getLocationInWindow(location2);
-
-            int left = location[0], top = location2[1], right = view.getWidth(),
-                    bottom = location[1] + view.getHeight();
-
-            // 判断点击位置是否在view布局范围内
-            if (view.getVisibility()==View.VISIBLE){
-                if (ev.getRawX() < left || ev.getRawX() > right
-                        || ev.getY() < top || ev.getRawY() > bottom) {
-                    // 在这里执行你的操作，我的是判断当前布局显示的话，隐藏掉
-                    view.setVisibility(View.GONE);
-                }
-            }
-        }
-        return super.dispatchTouchEvent(ev);
-    }
-
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -752,10 +591,6 @@ public class MainActivity extends AppCompatActivity
             case R.id.search_button:
                 searchButton();
                 break;
-            //点击下一页按钮
-//            case R.id.next_button:
-//                nextButton();
-//                break;
             case R.id.search_icon:
                 searchIconButton();
                 break;
@@ -809,23 +644,6 @@ public class MainActivity extends AppCompatActivity
         thread2=new Thread(runnable2);
         thread2.start();
     }
-
-    /**
-     * 点击下一页按钮
-     */
-    /*
-    public void nextButton() {
-        if (query != null && poiSearch != null && poiResult != null) {
-            if (poiResult.getPageCount() - 1 > currentPage) {
-                currentPage++;
-                query.setPageNum(currentPage);// 设置查后一页
-                poiSearch.searchPOIAsyn();
-            } else {
-                showToast("已经最后一页了");
-            }
-        }
-    }
-     */
 
     /**
      * 点击search icon
@@ -953,64 +771,6 @@ public class MainActivity extends AppCompatActivity
         dialog.show();
 
     }
-
-    /**
-     *
-     * @param s
-     * @param start
-     * @param count
-     * @param after
-     * 下面三个函数都是关于地点搜索框的
-     */
-    /*
-    @Override
-    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-        poiListView.setVisibility(View.GONE);
-    }
-
-    @Override
-    public void onTextChanged(CharSequence s, int start, int before, int count) {
-        String content=s.toString().trim();//获取自动提示输入框的内容
-        if ("".equals(content)) {
-            showToast("请输入关键词");
-//            aMap.clear();
-        }
-        else {
-            keyWord=content;
-            String city = nowCity;
-            if (poisitionIsChosen) {
-//                aMap.clear();
-                LatLng latLng = new LatLng(focusPoi.getLatitude(), focusPoi.getLongitude());
-                MarkerOptions markerOptions = new MarkerOptions()
-                        //必须，设置经纬度
-                        .position(latLng);
-                aMap.addMarker(markerOptions);
-                aMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 19));
-
-            }else{
-                doSearchQuery();
-            }
-            Log.i("城市", city);
-            Log.i("自动提示框", content);
-        }
-    }
-*/
-    /**
-     *在确认点击条目后，搜索栏会隐藏
-     */
-    /*
-    @Override
-    public void afterTextChanged(Editable s) {
-        if(poisitionIsChosen){
-            poiListView.setVisibility(View.GONE);
-            poisitionIsChosen=false;
-//            Log.i("afterTextChanged", "true");
-        }
-        else{
-            poiListView.setVisibility(View.VISIBLE);
-        }
-    }
-     */
 
     /**
      *post当前位置，获得病例信息
